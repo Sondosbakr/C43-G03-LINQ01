b01b6671c7903599a756b92f55711f2afe9f6075
Sondos <Sondosbakr81@gmail.com>
2025-02-11 14:06:43 +0200

    Add project files.

---
+++ Assignment01LINQ/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.WebSockets;
+using static Assignment01LINQ.ListGenerator;
+namespace Assignment01LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+        }
+    }
+}
