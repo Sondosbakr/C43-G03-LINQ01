c4ddc08ed7e339a70fb969351d3653c459e79563
Sondos <Sondosbakr81@gmail.com>
2025-02-11 14:16:56 +0200

    Restriction Operators - Question01

--- Assignment01LINQ/Program.cs
+++ Assignment01LINQ/Program.cs
@@ -6,7 +6,18 @@ namespace Assignment01LINQ
     {
         static void Main(string[] args)
         {
-            
+            #region LINQ - Restriction Operators
+            #region 1. Find all products that are out of stock.
+            var Result = ProductsList.Where(p => p.UnitsInStock >0);
+            Result = from p in ProductsList
+                     where p.UnitsInStock > 0
+                     select p;
+            foreach (var item in Result)
+            {
+                Console.WriteLine(item);
+            }
+            #endregion
+            #endregion
 
 
 
