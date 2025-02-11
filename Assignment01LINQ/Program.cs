bb1e9c82e44440f12cc96847bc5dd8c62bfd5bf1
Sondos <Sondosbakr81@gmail.com>
2025-02-11 15:12:37 +0200

    Restriction Operators - Question03

--- Assignment01LINQ/Program.cs
+++ Assignment01LINQ/Program.cs
@@ -19,14 +19,26 @@ namespace Assignment01LINQ
             #endregion
 
             #region 2. Find all products that are in stock and cost more than 3.00 per unit.
-            var Result = ProductsList.Where(p => p.UnitsInStock > 0 && p.UnitPrice > 3);
-            Result = from p in ProductsList
-                     where p.UnitsInStock > 0 && p.UnitPrice > 3
-                     select p;
+            //var Result = ProductsList.Where(p => p.UnitsInStock > 0 && p.UnitPrice > 3);
+            //Result = from p in ProductsList
+            //         where p.UnitsInStock > 0 && p.UnitPrice > 3
+            //         select p;
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+
+            #region 3. Returns digits whose name is shorter than their value.
+            String[] Arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            var Result = Arr.Select((name, index) => new { digit = index, length = name.Length })
+                .Where(x => x.length < x.digit).Select(x => x.digit);
+
             foreach (var item in Result)
             {
                 Console.WriteLine(item);
             }
+
             #endregion
             #endregion
 
